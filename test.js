951afc1ab7afd3d652aae67dc104a4faa7a07422
bya2 <byaa1972@gmail.com>
2021-08-16 15:17:37 +0900

    test: Example

---
+++ test.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+const getBreeds = async () => {
+  try {
+    return await axios.get('https://dog.ceo/api/breeds/list/all');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const countBreeds = async () => {
+  const breeds = await getBreeds();
+
+  console.log(Object.keys(breeds.data.message).length);
+  
+  if (breeds.data.message) {
+    console.log(`현재 강아지의 수는 ${Object.entries(breeds.data.message).length}입니다.`);
+  }
+};
+
+countBreeds();
