cbd15596a0a7177b559818f3f51bada22cc0a8b3
bya2 <byaa1972@gmail.com>
2021-08-17 14:03:39 +0900

    프로퍼티 img 추가, MongoDB 이용

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 
-const url = process.env.TARGET;
-const ulList = [];
+const db = require('./db');
+const Book = require('./models/Book');
 
-// Axios를 이용해서 비동기로 해당 URL의 HTML을 가져옴
+const url = process.env.TARGET;
+
 const getHtml = async url => {
   try {
     return await axios.get(url);
@@ -15,39 +16,28 @@ const getHtml = async url => {
 }
 
 getHtml(url).then(html => {
-  // 반환되는 Promise 객체를 cheerio를 이용해서 데이터를 가공
-  // HTML 문자열을 cheerio 객체로 반환
-  const $ = cheerio.load(html.data);
-  const $ulList = $('div#webtoon-list ul#webtoon-list-all').children('li'); // children: 해당되는 태그들의 배열을 반환
+  const $ = cheerio.load(html.data); // HTML 문자열을 cheerio 객체로 반환
+  const $ulList = $('div#webtoon-list ul#webtoon-list-all').children('li:eq(2)'); // children: 해당되는 태그들의 배열을 반환
 
-  // 주의!: JQuery의 Method는 콜백함수로 Arrow function 사용 불가능.
-  // 태그 배열을 순회하면서 콜백함수 실행
   $ulList.each(function(i, el) {
-    // Error: Request path contains unescaped characters.
-    // 해결: 요청 경로에 처리하지 못하는 문자열이 존재하므로 내장 함수인 encodeURI()를 이용
+    const title = $(this).find('span').text();
     const url = encodeURI($(this).find('a').attr('href'));
 
-    // 해당되는 인덱스에 정보 저장
-    ulList[i] = {
-      title: $(this).find('span').text(),
-      url: url
-    }
-
-    // 해당되는 태그 내 포함된 URL의 HTML을 가져옴
     getHtml(url).then(html => {
-      // HTML 문자열을 cheerio 객체로 반환
       const $ = cheerio.load(html.data);
       
-      // 해당되는 인덱스에 정보 저장
-      ulList[i] = {
-        score: $('div.view-comment').text().split(' '),
-        comments: $('div.view-comment span.orangered').text(),
-        good = $('div.view-content b#wr_good').text(),
-        person = $('div.view-comment').text().split(' ').pop().split('/')[0]
+      const info = {
+        title: title,
+        url: url,
+        img: $('div.view-title div.view-img img').attr('src'),
+        score: parseFloat($('div.view-comment').text().split('\n')[5]),
+        comments: parseInt($('div.view-comment span.orangered').text()),
+        good: parseInt($('b#wr_good').text()),
+        person: parseInt($('div.view-comment').text().split(' ').pop().split('\t')[0])
       }
 
-      // 출력
-      console.log(ulList[i]);
+      const bookDoc = new Book(info);
+      bookDoc.save(err => err ? console.error('Error while saving:\n', err.message) : console.log('Completed saving to database.'));
     });
   })
 });
