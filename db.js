aface87d63afcd1c1507d17ca2aae3e8ea419395
bya2 <byaa1972@gmail.com>
2021-08-17 16:38:09 +0900

    fix: MongoDB URI의 dotenv 환경 변수 작성

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/simple-crawling', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/simple-crawling', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
