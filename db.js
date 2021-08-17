094ed676928f8cc4957d70bbf30e5474d7e03e17
bya2 <byaa1972@gmail.com>
2021-08-17 13:57:22 +0900

    fix: ES6과 CommonJS 방식은 병행 불가 (Error: Cannot use import statement outside a module)

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/simple-crawling', { useNewUrlParser: true, useUnifiedTopology: true });
 
