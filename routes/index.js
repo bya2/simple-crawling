bf456759db27c2af9d448d807e6d78d71e7f49a2
bya2 <byaa1972@gmail.com>
2021-08-19 23:29:10 +0900

    write: 라우팅, 렌더링 및 객체를 hbs에 변수로 전달

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 
+const Books = require('../models/Book');
+
 const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const docs = await Books.find({});
+
+    res.status(200).render('index', {
+      title: 'Newtoki Romance',
+      subtitle: 'Ranking',
+      metaData: ['IMG', 'TITLE', 'SCORE', 'COMMENTS', 'GOOD', 'PERSON', 'UPDATED'],
+      docs: docs,
+    });
+
+  } catch (err) {
+    console.error('Error finding document:\n', err.message)
+  }
+});
 
 module.exports = router;
