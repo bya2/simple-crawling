e6f09393bd739372021db861f54f89b696d7b83d
bya2 <byaa1972@gmail.com>
2021-08-20 22:47:32 +0900

    HTML 디자인 변경

--- public/index.js
+++ public/index.js
@@ -1,4 +1,38 @@
-const clock = document.querySelector(".h1-clock");
+// Hide (Header-Inner)
+const headerInner = document.querySelector('.header-inner');
+
+let isInner = false;
+let displayInner = false;
+
+document.addEventListener('click', () => {
+  if (isInner) {
+    if (displayInner) {
+      headerInner.style.display = 'none';
+    } else {
+      headerInner.style.display = 'block';
+    }
+  }
+})
+
+document.addEventListener('scroll', () => {
+  const currScroll = document.documentElement.scrollTop;
+
+  console.log(currScroll);
+
+  if (!isInner && currScroll > 120) {
+    headerInner.style.position = 'fixed';
+    isInner = true;
+  }
+
+  if (isInner && currScroll === 0) {
+    headerInner.style.position = 'absolute';
+    isInner = false;
+  }
+});
+
+
+// Clock
+const clock = document.querySelector('.h1-clock');
 
 const putZero = num => {
   return num < 10 ? `0${num}` : num;
