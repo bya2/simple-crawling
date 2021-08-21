1ccc8256ba24c2e822a88aae1bd8c0e2e48d1d0c
bya2 <byaa1972@gmail.com>
2021-08-21 20:21:20 +0900

    Puppeteer, cheerio 모듈을 이용해서 크롤링하여 프로퍼티 추출

--- test.js
+++ test.js
@@ -1,33 +1,39 @@
-const axios = require('axios').default;
-const cheerio = require('cheerio');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
-const url = 'https://page.kakao.com/home?seriesId=52001198'
-
-const getHtml = async url => {
-  try {
-    return await axios.get(url);
-  } catch (err) {
-    throw console.error('Error in get method of axios:\n', err.message);
-  }
-}
-
-const loadData = async url => {
-  try {
-    const html = await getHtml(url);
-    const $ = cheerio.load(html.data);
-    return $;
-  } catch (err) {
-    console.error(`Error in loading data:\n${err.message}`);
-    return null;
-  }
-}
+const qs = require('./querySelector');
+const commentsPage = require('./page/commentsPage');
+const seriesPage = require('./page/seriesPage');
 
 (async () => {
-  const $ = loadData(url);
+  const url = process.env.TARGET_URL || `${process.env.HOST || 'https://sosul.network'}/series/comments/`;
+  const options = {
+    headless: true,
+  }
 
-  if ($) {
-    console.log(1);
-  } else {
-    console.log(2);
+  let browser;
+
+  try {
+    browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+
+    await page.setUserAgent(process.env.USER_AGENT || 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.0 Safari/537.36');
+
+    await page.goto(url);
+    console.log(await page.evaluate('navigator.userAgent'));
+    await page.waitForSelector(qs.comments.gridLayout.items, { timeout: 10000 });
+
+    commentsPage(page)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => console.error(`Error in comments page:\n${err.message}`));
+
+    await page.waitForTimeout(3000);
+    await page.close();
+  } catch (err) {
+    console.error(`Error in processing:\n${err.message}`);
+  } finally {
+    await browser.close();
   }
 })();
