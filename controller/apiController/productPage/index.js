a8f380e1ff9294d1ba7466cbfc108a37d0bed752
bya2 <byaa1972@gmail.com>
2021-08-28 12:18:59 +0900

    fix: href->url & TEST OK

--- controller/apiController/productPage/index.js
+++ controller/apiController/productPage/index.js
@@ -1,37 +1,27 @@
 const { productPage } = require('../selectors');
+const { comment, product } = productPage;
 
 module.exports = {
-  // 해당 작품의 페이지에서 해당 작품이 서비스되는 플랫폼 정보를 스크랩하는 함수
+  // // // 해당 작품의 페이지에서 해당 작품이 서비스되는 플랫폼 정보를 스크랩하는 함수 // // //
 
-  // **코드 설명**
   // 셀렉터에 대응하는 태그 목록의 HTML을 추출
   // HTML에서 플랫폼의 이름과 링크 정보를 객체로 만들고, 객체로 이루어진 배열 생성
   getPlatforms: async page => {
-    const platforms = productPage.product.platforms;
+    const platforms = product.platforms;
 
-    // const els = await page.$$(platforms);
-
-    // const arr = await Promise.all(els.map(el => page.evaluate(e => ({ name: e.innerHTML, href: e.href }), el)));
-    // return arr;
-
-    const arr = await page.evaluate(el => ({ name: el.innerHTML, href: el.href }), await page.$$(platforms));
+    const els = await page.$$(platforms);
+    const arr = await Promise.all(els.map(el => page.evaluate(e => ({ name: e.innerHTML, url: e.href }), el)));
     return arr;
-  
-    // const platforms = [];
-    // await Promise.all(els.forEach(async el => {
-    //   platforms.push(await page.evaluate(e => ({ name: e.innerHTML, href: e.href }), el));
-    // }));
-    // return platforms;
   },
+  // TEST OK
 
-  // 해당 작품의 페이지에서 가장 공감되는 Comment의 정보를 스크랩하는 함수
 
-  // **코드 설명**
+  // // // 해당 작품의 페이지에서 가장 공감되는 Comment의 정보를 스크랩하는 함수 // // //
+
   // 생성된 페이지를 전달받고, 셀렉터마다 map함수를 통해 HTML을 추출
   // 프로미스를 병렬적으로 처리해서 웹 페이지 로딩에 의해 순서에 따라 시간 제약을 받지 않고 배열 생성
   // 셀렉터 키 배열과 프로미스 배열로 reduce함수를 통해 객체 생성
   getBestComment: async page => {
-    const comment = productPage.comment;
     const { commentId, rate } = comment;
 
     const promises = await Promise.all(Object.values(comment).map(el => {
@@ -45,4 +35,5 @@ module.exports = {
 
     return obj;
   },
+  // TEST OK
 }
